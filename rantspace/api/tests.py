06e05405cdc27217442fa94e0cf7e97a204f71dd
gabrielchase <gchase.patron@gmail.com>
2017-08-09 22:15:06 +0800

    Tests for creating a user

---
+++ rantspace/api/tests.py
@@ -0,0 +1,79 @@
+from django.test import TestCase
+
+from api.models import (
+    UserProfile,
+    UserProfileManager
+)
+
+
+manager = UserProfileManager()
+# UserProfile = UserProfile()
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.old_count = UserProfile.objects.count()
+
+        self.user_attributes = {
+            'email': 'test@test.com', 
+            'surname': 'test',
+            'given_name': 'test',
+            'password': 'password'
+        }
+        
+        UserProfile.objects.create_user(
+            email=self.user_attributes.get('email'),
+            surname=self.user_attributes.get('surname'),
+            given_name=self.user_attributes.get('given_name'),
+            password=self.user_attributes.get('password')
+        )
+
+    def test_model_saves_user(self):
+        new_count = UserProfile.objects.count()
+        new_user = UserProfile.objects.get(email='test@test.com')
+
+        self.assertNotEqual(self.old_count, new_count)
+        self.assertEqual(bool(new_user.id), True)
+        self.assertEqual(new_user.email, self.user_attributes.get('email'))
+        self.assertNotEqual(new_user.password, self.user_attributes.get('password'))
+        self.assertEqual(new_user.surname, self.user_attributes.get('surname'))
+        self.assertEqual(new_user.given_name, self.user_attributes.get('given_name'))
+        self.assertEqual(new_user.is_active, True)
+        self.assertEqual(new_user.is_staff, False)
+
+
+    def test_model_normalizes_email(self):
+        normalize_email_attributes = {
+            'email': 'test@TeSt2.cOm', 
+            'surname': 'test',
+            'given_name': 'test',
+            'password': 'password'
+        }
+
+        user = UserProfile.objects.create_user(
+            email=normalize_email_attributes.get('email'),
+            surname=normalize_email_attributes.get('surname'),
+            given_name=normalize_email_attributes.get('given_name'),
+            password=normalize_email_attributes.get('password')
+        )
+
+        self.assertEqual(user.email, 'test@test2.com')
+
+    def test_model_raises_error_when_email_not_provided(self):
+        no_email_attributes = {
+            'surname': 'test',
+            'given_name': 'test',
+            'password': 'password'
+        }
+
+        with self.assertRaises(ValueError) as context:
+            UserProfile.objects.create_user(
+                email=None,
+                surname=no_email_attributes.get('surname'),
+                given_name=no_email_attributes.get('given_name'),
+                password=no_email_attributes.get('password')
+            )
+
+            self.assertTrue('Users must provide an email address' in context.exception)
+
+
+
