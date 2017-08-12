bf46d4f267a251f32db4900782ac2b4f5d3e5cab
gabrielchase <gchase.patron@gmail.com>
2017-08-12 14:47:38 +0800

    Add token based login

--- msme_pos/api/urls.py
+++ msme_pos/api/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
 from api.views import (
-    UserProfileViewSet
+    UserProfileViewSet,
+    LoginViewSet
 )
 
 router = DefaultRouter()
 
 router.register('profile', UserProfileViewSet)
+
+# Set base_name if its not a ModelViewSet
+router.register('login', LoginViewSet, base_name='login') 
 
 urlpatterns = [
     url(r'', include(router.urls))
--- msme_pos/api/views.py
+++ msme_pos/api/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from api.models import UserProfile
 from api.serializers import UserProfileSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """ Handles creating, updating, and deleting UserProfile"""
+    """ Handles creating, updating, and deleting UserProfile """
 
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
@@ -19,3 +21,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         'address', 'city', 'state'
     )
     
+
+class LoginViewSet(viewsets.ViewSet):
+    """ Checks email and password and returns an authtoken """
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """ Use the ObtainAuthToken APIView to validate and create a token """
+
+        return ObtainAuthToken().post(request)
