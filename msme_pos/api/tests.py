c9ee77000daab0ac67b9202a47d91f7f0b1dff70
gabrielchase <gchase.patron@gmail.com>
2017-08-13 09:58:21 +0800

    Fix retrieve test and add authorization tests

--- msme_pos/api/tests.py
+++ msme_pos/api/tests.py
@@ -240,6 +240,23 @@ class ViewTestCase(TestCase):
             format='json'
         )
 
+        self.created_user = self.api_response.json()
+
+        self.user_login_data = {
+            'username': self.user_data.get('email'),
+            'password': self.user_data.get('password'),
+        }
+
+        self.api_login_response = self.client.post(
+            reverse('api:login-list'),
+            self.user_login_data,
+            format='json'
+        )
+
+        self.login_token = self.api_login_response.json().get('token')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.login_token)
+
+
     def test_api_can_create_user(self):
         new_count = UserProfile.objects.count()
 
@@ -260,23 +277,24 @@ class ViewTestCase(TestCase):
             self.assertTrue(user.get('id'))
 
     def test_api_can_get_user(self):
-        user = UserProfile.objects.get()
-
-        self.api_response = self.client.get(
-            reverse('api:profile-detail', kwargs={'full_business_name': user.full_business_name}),
+        api_response = self.client.get(
+            reverse(
+                'api:profile-detail', 
+                kwargs={'full_business_name': self.created_user.get('full_business_name')}
+            ),
             format='json'
         )
 
-        self.assertEqual(self.api_response.status_code, status.HTTP_200_OK)
-        self.assertTrue(user.id)
-        self.assertEqual(self.api_response.json().get('email'), user.email)
-        self.assertEqual(self.api_response.json().get('business_name'), user.business_name)
-        self.assertEqual(self.api_response.json().get('identifier'), user.identifier)
-        self.assertEqual(self.api_response.json().get('full_business_name'), user.get_full_name())
-        self.assertEqual(self.api_response.json().get('owner_surname'), user.owner_surname)
-        self.assertEqual(self.api_response.json().get('owner_given_name'), user.owner_given_name)
-        self.assertFalse(self.api_response.json().get('is_superuser'))
-        self.assertFalse(self.api_response.json().get('is_staff'))
+        self.assertEqual(api_response.status_code, status.HTTP_200_OK)
+        self.assertTrue(api_response.json().get('id'))
+        self.assertEqual(api_response.json().get('email'), self.created_user.get('email'))
+        self.assertEqual(api_response.json().get('business_name'), self.created_user.get('business_name'))
+        self.assertEqual(api_response.json().get('identifier'), self.created_user.get('identifier'))
+        self.assertEqual(api_response.json().get('full_business_name'), '{}-{}'.format(self.created_user.get('business_name'), self.created_user.get('identifier')))
+        self.assertEqual(api_response.json().get('owner_surname'), self.created_user.get('owner_surname'))
+        self.assertEqual(api_response.json().get('owner_given_name'), self.created_user.get('owner_given_name'))
+        self.assertFalse(api_response.json().get('is_superuser'))
+        self.assertFalse(api_response.json().get('is_staff'))
 
     def test_api_can_update_user(self):
         user_to_be_updated = UserProfile.objects.get()
@@ -329,3 +347,44 @@ class ViewTestCase(TestCase):
         )
         
         self.assertEqual(api_response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_api_cannot_update_without_token(self):
+        no_token_client = APIClient()
+        user_to_be_updated = UserProfile.objects.get()
+
+        new_user_data = {
+            'email': 'business_email_2@email.com',
+            'password': user_to_be_updated.password,
+            'business_name': 'business2',
+            'identifier': 'street2',
+            'owner_surname': 'owner2',
+            'owner_given_name': 'owner2',
+            'address': 'address2',
+            'city': 'city2',
+            'state': 'state2'
+        }
+
+        api_response = no_token_client.put(
+            reverse(
+                'api:profile-detail', 
+                kwargs={'full_business_name': user_to_be_updated.full_business_name}
+            ),
+            new_user_data,
+            format='json'
+        )
+
+        self.assertEqual(api_response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_api_cannot_delete_without_token(self):
+        no_token_client = APIClient()
+        user_to_be_deleted = UserProfile.objects.get()
+
+        api_response = no_token_client.delete(
+            reverse(
+                'api:profile-detail', 
+                kwargs={'full_business_name': user_to_be_deleted.full_business_name}
+            ),
+            format='json'
+        )
+        
+        self.assertEqual(api_response.status_code, status.HTTP_401_UNAUTHORIZED)
