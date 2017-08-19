483e434664b574f0188f608b781241108f768e15
gabrielchase <gchase.patron@gmail.com>
2017-08-19 15:12:42 +0800

    Make `permissions.py` more explicit and add permission for geting own `OrderItem`

--- msme_pos/api/permissions.py
+++ msme_pos/api/permissions.py
@@ -4,14 +4,21 @@ from rest_framework import permissions
 class GetAndUpdateOwnProfile(permissions.BasePermission):
     """ Allow users to edit their own profile """
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, user_profile):
         """ Check if user is trying their update or delete their own profile """
 
-        return obj.id == request.user.id or request.user.is_superuser
+        return user_profile.id == request.user.id or request.user.is_superuser
 
 
 class GetAndUpdateOwnMenuItem(permissions.BasePermission):
     """ Allow user to update or delete their own menu item """
 
-    def has_object_permission(self, request, view, obj):
-        return obj.user_profile == request.user or request.user.is_superuser
+    def has_object_permission(self, request, view, menu_item):
+        return menu_item.user_profile == request.user or request.user.is_superuser
+
+
+class GetAndUpdateOwnOrderItem(permissions.BasePermission):
+    """ Allow user to update or delete their own menu item orders"""
+
+    def has_object_permission(self, request, view, order):
+        return order.menu_item.user_profile == request.user or request.user.is_superuser
